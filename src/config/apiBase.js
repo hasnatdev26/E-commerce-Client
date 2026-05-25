35096b09abef250a33e2334b05fc4b84c38c55df
hasnat evan <tasnovaazimgmail.com>
2026-05-25 07:26:25 -0600

    add fole

--- src/config/apiBase.js
+++ src/config/apiBase.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://e-commerce-server-5i883y43r-hasnatdevs-projects.vercel.app";
+  import.meta.env.VITE_API_URL || "https://e-commerce-server-fawn-mu.vercel.app";
 
 export default API_BASE_URL;
